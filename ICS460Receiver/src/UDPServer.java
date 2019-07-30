6ddcbca2468f70ccf7e0e1ac4d9b84b7755b5328
Troy Novak <novak_troy@yahoo.com>
2019-07-30 00:09:40 -0500

    Finished javadocs

--- ICS460Receiver/src/UDPServer.java
+++ ICS460Receiver/src/UDPServer.java
@@ -1,5 +1,3 @@
-// receiver 2 in my projects
-
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,19 +6,22 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
-
+/**
+ * Receives datagram packets from a sender using UDP stop and wait protocol.
+ * Involves sending Acknowledments back to sender so the sender can send next
+ * packet.
+ * 
+ * @author Troy Novak, Brent Windham, Abdullah Almamun, Yuan Lu
+ *
+ */
 public class UDPServer {
 	
 	private final String RECV = "RECV";
 	private final String DUPL = "DUPL";
 	private final String CRPT = "CRPT";
-	private final String SEQ_ERR = "!Seq";//WONT BE USED
-	private final String ACKKNOWLEDGE = "ACK";
 	private final String SENT = "SENT";
 	private final String ERR = "ERR";
 	private final String DROP = "DROP";
-	private final String TIMEOUT = "TimeOut";//Server cant timeout.  Doesnt Make sense
-	private final String SEND_MESSAGE = "SENDing";
 	
 	// instantiate constants
 	private final int ACK_PORT = 9587;
@@ -43,19 +44,24 @@ public class UDPServer {
 	private float badPackets;
 
 	
-	private short chksum = 0;
+	//private short chksum = 0;
 	private short len = ACK_SIZE;
 	private int ackno = 1;
 	private int seqno = 0;
 	private boolean isDuplicate;
 	private int currentWrite = -1;
 	
-	
 	// added for ack testing
 	private InetAddress address;
 	private boolean goodPacket = true;
 	
-	// class constructor
+	/**
+	 * Constructor for UDPServer class, instantiates necessary variables.
+	 * 
+	 * @param address
+	 * @param portNum
+	 * @param badPackets
+	 */
 	public UDPServer(String address, int portNum, float badPackets) {
 		try {
 			
@@ -63,23 +69,25 @@ public class UDPServer {
 			// (socket to receive does need port as parameter)
 			this.packetSocket = new DatagramSocket(portNum);
 			
-			// open socket used to receive acknowledgments\             //This says receive but I think its suppposed to be send
-			// (socket to send doesn't need port as parameter)
+			// open socket used to send acknowledgments
 			this.ackSocket = new DatagramSocket();
 			
 			// specify host's IP address
 			this.address = InetAddress.getByName(address);
 			
-			// specify the port number for receiver to find packets in
-			this.receiverPort = portNum;
-			
 			this.badPackets = badPackets;
 			
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 	}
 	
+	/**
+	 * Method used to receive packets and implements part of a proxy to
+	 * potentially corrupt the acknowledgements being sent back.
+	 */
 	public void receive() {
 		
 		int numPackets = 0;		// packet counter
@@ -88,7 +96,6 @@ public class UDPServer {
 		// open byte output stream to allow file to be compiled when received
 		ByteArrayOutputStream byteOStream = new ByteArrayOutputStream();
 		
-		// add + HEADER_SIZE when ready
 		buf = new byte[BUFFER_SIZE + HEADER_SIZE];		// initialize buffer array for incoming data to be held
 		header = new byte[HEADER_SIZE];		// initialize buffer array for header data
 		fileData = new byte[BUFFER_SIZE];	// initialize buffer array for file data
@@ -107,12 +114,7 @@ public class UDPServer {
 
 				long timeReceived = dateReceived.getTime();
 
-			//	System.out.println("Receiving packet...");
-				
-				
 				segmentPacketArray(dgPacket);
-//				System.out.println("header buffer being received- " + Arrays.toString(header));//This just shows the header so we can check if corrupt
-
 				
 				//Getting Bytes so we can run tests
 				byte[] test = dgPacket.getData();
@@ -124,56 +126,60 @@ public class UDPServer {
 				//creating the ackno
 				ByteBuffer acknoBuffer = ByteBuffer.wrap(test,4,4);
 				int tempAckno = acknoBuffer.getInt();
-//				System.out.println("tempAckno: " + tempAckno);
-				
 				
 				//creating the seqno
 				ByteBuffer seqnoBuffer = ByteBuffer.wrap(test,8,4);
 				seqno = seqnoBuffer.getInt();
-//				System.out.println("ByteBuffer.array(): " + Arrays.toString(seqnoBuffer.array()));
-//				System.out.println("seqno: " + seqno);
-				
-				//Printing Corruption
-//				System.out.println("chksum: " + test[1] + "\n");
 
 				//Testing if packet is corrupt //This comes last because Corrupt files dont care if there is a duplicate because seqno could be affected
 				//Testing if packet is a duplicate
 				//Setting Ackno to Next Packet needed regardless of duplicate //no need to reset seqno because we update it when we receive our packet. Not necessary to keep value
-				if (chksum == 1) {    					
+				if (chksum == 1) {   
+					
 					//goodPacket = false;
 					receivedPacketCondition = CRPT;
+					
+					// test for duplicate ack
 					if (tempAckno == ackno) {
+						
 						isDuplicate = true;
 						System.out.println(DUPL + " " + timeReceived + " " + seqno + " " + receivedPacketCondition);
-
+						
+					// if not a duplicate
 					} else {
+						
 						isDuplicate = false;
 
 						System.out.println(RECV + " " + timeReceived + " " + seqno + " " + receivedPacketCondition);
+						
 					}
+					
+					// set ackno to next ackno
 					this.ackno = tempAckno;
 
 				} else {
+					
 					receivedPacketCondition = RECV;
 					
+					// test for duplicate ack
 					if (tempAckno == ackno) {
+						
 						isDuplicate = true;
 
 						//receivedPacketCondition = DUPL;
 						System.out.println(DUPL + " " + timeReceived + " " + seqno + " " + receivedPacketCondition);
 
 					} else {
+						
 						isDuplicate = false;
 
 						System.out.println(RECV + " " + timeReceived + " " + seqno + " " + receivedPacketCondition);
 
 					}
-					this.ackno = tempAckno;
-//					System.out.println();
-					acknowledge();
 					
-					// appends packet's data to output stream
-					//byteOStream.write(fileData);
+					this.ackno = tempAckno;
+					
+					acknowledge();
 					
 					numPackets++;						// increments packet counter
 					
@@ -181,11 +187,14 @@ public class UDPServer {
 					// if so, negate loop's pre-test condition
 					// (converts data to String, checks if equal to termination code)
 					if(new String(dgPacket.getData(), 0, dgPacket.getLength()).trim().equals("stop")){
+						
 						running = false;
 						byteOStream.close();// negates loop's pre-test condition
+						
 					}
 					// appends packet's data to output stream
 					if (seqno > currentWrite) {
+						
 						byteOStream.write(fileData);
 						currentWrite = seqno;
 
@@ -194,45 +203,14 @@ public class UDPServer {
 				
 				goodPacket = true;
 
-				
-				
 				goodPacket = true;
 
-				
-				
-				// packet trimming happens here
-				segmentPacketArray(dgPacket);
-				
-				
-				
-				
-				//System.out.println("This is the header" + Arrays.toString(header));    Just tests
-				
-				
-				
-				
-				
-				//System.out.println("This is the file data" + Arrays.toString(fileData));
-
-				// for testing purposes
-				//System.out.println(Arrays.toString(header));
-
-				//System.out.println(Arrays.toString(fileData));
-
-				
-				// implement when ready
-				//byteOStream.write(trimData(dgPacket));
-				
-			
-				
-				// output format [packet #][start byte offset][end byte offset]
-//				System.out.println(String.format("[%d][%d][%d]", seqno-1, (seqno-1) * BUFFER_SIZE, (seqno-1) * BUFFER_SIZE + fileData.length) + "possible end\n");
-				
-				
 				buf = new byte[BUFFER_SIZE + HEADER_SIZE];		// clears buffer
 				
 			}catch(IOException e) {
+				
 				e.printStackTrace();
+				
 			}
 		}
 		
@@ -240,15 +218,25 @@ public class UDPServer {
 		
 		// close socket and byte output stream
 		try {
+			
 			packetSocket.close();
 			ackSocket.close();
 			byteOStream.close();
+			
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 	
 	}
 	
+	/**
+	 * Writes the bytestream to server's storage so it can be accessed by
+	 * the user.
+	 * 
+	 * @param byteOStream
+	 */
 	public void writeFileToDisk(ByteArrayOutputStream byteOStream) {
 		// write compiled packets(file) to system
 		try {
@@ -267,54 +255,58 @@ public class UDPServer {
 					
 				
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 		
 	}
 	
-	
-	
-	
-	
-	// just testing so far
+	/**
+	 * Sends acknowledgment back to sender. Checks for duplicate
+	 * acknowledgments, as well as makes sure that the acknowledgment hasn't
+	 * been dropped by the server.
+	 */
 	public void acknowledge() {
 		
 		try {
+			
 			createAckPacket();
+			
 			DatagramPacket acknowledge = new DatagramPacket(ackPacket, ackPacket.length, address, ACK_PORT);
 			Date ackDateSent = new Date();
 
 			long ackSendTime = ackDateSent.getTime();
+			
 			if (ackPacketCondition != DROP) {
-//				ackSocket.send(acknowledgeWithCondition);
+				
 				ackSocket.send(acknowledge);
 				
-//				System.out.println("Ack being sent- " + Arrays.toString(ackPacket));//This just shows the header so we can check if corrupt
-
-//				System.out.println("chksum being sent to Client: " + ackPacket[1]);
-//				System.out.println("ackno being sent to client: " + ackPacket[7]);
-//				System.out.println("Sending Ack to Client...\n");
 			}
 			
 			if (isDuplicate) {
+				
 				System.out.println("ReSend. ACK " + seqno + " " + ackSendTime + " " + ackPacketCondition);
 
 			} else {
+				
 				System.out.println("SENDing ACK " + seqno + " " +ackSendTime + " " + ackPacketCondition);
 
 			}
+			
 			ackPacketCondition = SENT;
 		
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 		
 	}
 	
-	
-	
-	
-	
+	/**
+	 * Creates the acknowledgment packet to be sent back to the sender.
+	 */
 	public void createAckPacket() {
 		ByteBuffer buf = ByteBuffer.wrap(ackPacket);
 		
@@ -347,46 +339,14 @@ public class UDPServer {
 		
 	}
 	
-	
-	
-	
-	public void createAckPacket() {
-		ByteBuffer buf = ByteBuffer.wrap(ackPacket);
-		
-		// create and add chksum header
-		byte[] chksumHeader = new byte[2];
-		ByteBuffer chksumBuf = ByteBuffer.wrap(chksumHeader);
-		chksumBuf.putShort(proxy());
-		chksumBuf.rewind();
-		
-		// create and add len header
-		byte[] lenHeader = new byte[2];
-		ByteBuffer lenBuf = ByteBuffer.wrap(lenHeader);
-		lenBuf.putShort(len);
-		lenBuf.rewind();
-		
-		// create and add ackno header
-		byte[] acknoHeader = new byte[4];
-		ByteBuffer acknoBuf = ByteBuffer.wrap(acknoHeader);
-		acknoBuf.putInt(ackno);
-		acknoBuf.rewind();
-		
-		// add chksum header to full header
-		buf.put(chksumHeader);
-		// add len header to full header
-		buf.put(lenHeader);
-		// add ackno header to full header
-		buf.put(acknoHeader);
-		
-		buf.rewind();
-		
-	}
-	
-	
-	
-	
-	
+	/**
+	 * Segments out incoming packets from sender so headers can be checked
+	 * for corruption.
+	 * 
+	 * @param packet
+	 */
 	public void segmentPacketArray(DatagramPacket packet) {
+		
 		byte[] buffer = packet.getData();
 		header = Arrays.copyOfRange(buffer, 0, HEADER_SIZE);
 		acknoHeader = Arrays.copyOfRange(buffer, 4, 8);
@@ -394,9 +354,13 @@ public class UDPServer {
 		
 	}
 	
-	
-	
-	
+	/**
+	 * Determines if an acknowledgment packet has been either dropped or
+	 * corrupted.
+	 * 
+	 * @return 0 if packet is not corrupt
+	 * @return 1 if packet has been corrupt
+	 */
 	public short proxy() {
 		
 		// decides if packet is corrupt, delayed, dropped, etc?
@@ -405,27 +369,26 @@ public class UDPServer {
 		Random rng = new Random();
 		int chance = rng.nextInt((int)(100));
 		
-		
 		int badPacketsInt = (int) (badPackets * 100);
 		int badPacketsPercentage = (int) (badPacketsInt / 2);
-		
 		
 		// corrupt packet
 		if(chance < badPacketsPercentage) {
 	
 			ackPacketCondition = ERR;
 			return 1;
+			
 		}
 		// drop packet
 		else if(chance > 100 - badPacketsPercentage) {
+			
 			ackPacketCondition = DROP;
 			return 0;
+			
 		}
-		
 		
 		return 0;
 		
 	}
-	
 	
 }
--- ICS460Sender/src/UDPClient.java
+++ ICS460Sender/src/UDPClient.java
@@ -1,5 +1,3 @@
-// sender 2 in git ** works properly **  **TEST COMMITT ***
-
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,20 +8,19 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Random; 
 
+/**
+ * Sends datagram packets to  a receiver using UDP stop and wait protocol.
+ * 
+ * @author Troy Novak, Brent Windham, Abdullah Almamun, Yuan Lu
+ *
+ */
 public class UDPClient {
 	
 	// Yuan
 	// output string constants
-	private final String SEND_FIRST_MESSAGE = "SENDing ";
-	private final String RESEND_MESSAGE = "ReSend. ";
 	private final String SENT = "SENT";
 	private final String DROP = "DROP";
 	private final String ERR= "ERR";
-	private final String ACK_RCVD = "AckRcvd";
-	private final String DUPL_ACK = "DuplAck";
-	private final String ERR_ACK = "ErrAck";
-	private final String MOVE_WINDOW = "MoveWnd";
-	private final String TIMEOUT = "TimeOut";
 	
 	// instantiate constants
 	private final int ACK_PORT = 9587;
@@ -37,30 +34,31 @@ public class UDPClient {
 	private DatagramSocket packetSocket;
 	private DatagramSocket ackSocket;
 	private InetAddress address;
-	private byte[] buf;
+	//private byte[] buf;
 	
 	private byte[] fileBuffer;		// used for file data
 	private byte[] stopBuffer;		// used for stop message
 	private byte[] ackBuffer;		// used for ack packet
 	private byte[] headerBuffer;	// used for header data
-	
 	private byte[] packetBuffer;	// used for entire packet
 	
-	
-	private short chksum = 0;
+	//private short chksum = 0;
 	private short len = HEADER_SIZE;
 	private int ackno = 1;
 	private int seqno = 0;
-	//private boolean dropped = true;
 	private String condition = "";
 	Date date = new Date();
 	int numPackets = 0;		// packet counter
 
-	
-	
-	
-	
-	// class constructor
+	/**
+	 * Constructor for UDPClient class, instantiates necessary variables.
+	 * 
+	 * @param address
+	 * @param portNum
+	 * @param bufSize
+	 * @param timeout
+	 * @param badPackets
+	 */
 	public UDPClient(String address, int portNum, int bufSize, int timeout, float badPackets) {
 		try {
 			
@@ -88,23 +86,26 @@ public class UDPClient {
 			this.percentBadPackets = badPackets;
 			
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 	}
 	
-	
-	
-	
-	// will send file using UDP DatagramPackets
+	/**
+	 * Method used to send packets and implements part of a proxy to
+	 * potentially corrupt the packets being sent.
+	 * 
+	 * @param file
+	 */
 	public void sendFile(String file) {
-		
 		
 		try {
 			
 			// open file input stream with file to be read into buffer
 			InputStream iStream = new BufferedInputStream(new FileInputStream(file));
 			
-			buf = new byte[bufferSize];		// initialize buffer array for data to be read to
+			//buf = new byte[bufferSize];		// initialize buffer array for data to be read to
 			fileBuffer = new byte[bufferSize];		// initialize buffer array for data to be read to
 			stopBuffer = new byte[bufferSize];		// initialize buffer array for stop message
 			ackBuffer = new byte[bufferSize];		// initialize buffer array for ack data
@@ -112,24 +113,8 @@ public class UDPClient {
 			
 			packetBuffer = new byte[headerBuffer.length + fileBuffer.length];	// add + HEADER_SIZE
 			
-			
 			// pre-test reads file from input stream and writes BUFFER_SIZE number of bytes to buffer to be sent in packets
 			while(iStream.read(fileBuffer) != -1) {
-				
-				
-				
-				// for testing
-				//System.out.println(Arrays.toString(packetBuffer));
-				
-				
-				
-				
-				// create new packet each iteration to be sent with(packet data, packet length, destination address, destination port)
-//				headerBuffer = createHeader();
-//				addHeader();
-//				DatagramPacket dgPacket = new DatagramPacket(packetBuffer, packetBuffer.length, address, receiverPort);
-				
-				
 				
 				//This will take good packet and return a packet that might be corrupted or dropped
 				//so we need to figure out where this needs to take place whether it is in the send function or before
@@ -137,80 +122,51 @@ public class UDPClient {
 				addHeader();
 				DatagramPacket dgPacketWithCondition = new DatagramPacket(packetBuffer, packetBuffer.length, address, receiverPort);
 				
-		
-				
-				// send packet via open socket //I think this the best solution to stop a dropped packet
+				// send packet via open socket as long as not dropped
 				if (condition != DROP) { 
-					packetSocket.send(dgPacketWithCondition); //This could be the best place to put the error chance but we need to know what the error is too
 					
-//					packetSocket.send(dgPacket);
+					packetSocket.send(dgPacketWithCondition);
 					
-//					packetSocket.send(dgPacket);
-					
-				}	//															   Sequence#   startByte	           EndByte	 ALSO NEEDS TIMESTAMP AND CONDITION
-					Date packetDateSent = new Date();
-					long time = packetDateSent.getTime();
-					System.out.println(String.format("%s %d %d:%d %s","SENDing", seqno, numPackets * bufferSize, numPackets * bufferSize + fileBuffer.length,time,condition));
-//					System.out.println("header buffer being sent- " + Arrays.toString(headerBuffer));//This just shows the header so we can check if corrupt
-					condition = SENT;
+				}	//	Sequence#   startByte	           EndByte	 ALSO NEEDS TIMESTAMP AND CONDITION
 				
-		
+					Date packetDateSent = new Date();
+					
+					long time = packetDateSent.getTime();
+					
+					System.out.println(String.format("%s %d %d:%d %s","SENDing", seqno, numPackets * bufferSize, numPackets * bufferSize + fileBuffer.length,time,condition));
+					
+					condition = SENT;
 				
 				//This is the response for when no acknowledgement is received. This occurs after timeout or Corrupt Ack.
 				while (receivedAck() == false) { 
 					
-					
-					
-					//Run Proxy again because Resend can also be corrupt of dropped
+					//Run Proxy again because Resend can also be corrupt or dropped
 					headerBuffer = createHeader();
 					addHeader();
 					DatagramPacket dgPacketResendWithCondition = new DatagramPacket(packetBuffer, packetBuffer.length, address, receiverPort);
 					
-					
 					//Send non dropped packets
 					if (condition != DROP) { 
-						packetSocket.send(dgPacketResendWithCondition); //This could be the best place to put the error chance but we need to know what the error is too
+						
+						packetSocket.send(dgPacketResendWithCondition);
+						
 					}
 					
-					//Creating array to show header
-				//	System.out.println("header buffer being ReSent- " + Arrays.toString(headerBuffer));//This just shows the header so we can check if corrupt
-
-					
-					//Displaying 
-//					System.out.println("Should be header of ReSend Packet" + Arrays.toString(bb.array()));
-//
-//					byte oneTest = 1;
-//					
-//					System.out.println("dgPacketResendWithCondition[1] " + test[1]);
-//					dgPacket.setData(data);
-//					packetSocket.send(dgPacket);
 					Date packetDateReSend = new Date();
-
 					long timeResend = packetDateReSend.getTime();
-					//System.out.println(String.format("[%s][%d][%d][%d]","ReSend", numPackets, numPackets * bufferSize, numPackets * bufferSize + fileBuffer.length,timeResend,condition));
-//					System.out.println("header buffer - " + Arrays.toString(packetBuffer));
-//					System.out.println("dgPacket data" + dgPacket.getDa);
+					
 					System.out.println(String.format("%s %d %d:%d %s","ReSend.", seqno, numPackets * bufferSize, numPackets * bufferSize + fileBuffer.length,timeResend,condition));
 
 					condition = SENT;
+					
 				}
-				
-				//Happens once Good Ack comes back
-				//System.out.println("ack received, packet successfully sent" + "\n");
 				
 				//increases What packet we are sending
 				numPackets++;		//increment packet counter
 				seqno++;
 				ackno++;
-
-				
-				
-
 				
 			}
-			
-			// for testing
-			//System.out.println("packetBuffer: " + Arrays.toString(packetBuffer) + "\n");
 			
 			// send termination code
 			stopBuffer = "stop".getBytes();
@@ -222,81 +178,77 @@ public class UDPClient {
 			ackSocket.close();
 			iStream.close();
 			
-			
 		}catch(IOException e) {
+			
 			e.printStackTrace();
+			
 		}
 		
 	}
 	
-	
-	
+	/**
+	 * Determines if an acknowledgment packet has been either dropped or
+	 * corrupted.
+	 * 
+	 * @return 0 if packet is not corrupt
+	 * @return 1 if packet has been corrupt
+	 */
 	public short proxy() {
-		
-		
-		
 		
 		//creates random number
 		Random rng = new Random();
 		int chance = rng.nextInt((int)(100));
-		
-		
-		int badPackets = (int) (percentBadPackets * 100);
-		int badPacketsPercentage = badPackets / 2;
-		
 		
 		int badPackets = (int) (percentBadPackets * 100);
 		int badPacketsPercentage = badPackets / 2;
 		
 		// corrupt packet
 		if(chance < badPacketsPercentage) {
+			
 			condition = ERR;
 			return 1;
 
 		}
 		// drop packet
 		else if(chance > 100 - badPacketsPercentage) {
+			
 			condition = DROP;
 			return 0;
+			
 		}
-		
 		
 		return 0;
 		
 	}
 	
-	// added for ack testing
-	//Need lots of Daylns help here
-	//Need to figure out how to do Timeout correctly
-	//I dont understand the timing of this.  When does it start? When does it end?
-	//Also need to test here for corruption
+	/**
+	 * Receives acknowledgments from the receiver and checks to make sure it
+	 * isn't corrupt. Implements timeout logic in case acknowledgment is never
+	 * received.
+	 * 
+	 * @return false if acknowledgment is corrupt or timeout occurs
+	 * @return true if acknowledgment is successfuly received
+	 */
 	public boolean receivedAck() {
 		
 		DatagramPacket acknowledged = new DatagramPacket(ackBuffer, ackBuffer.length);
 		
 		try {
+			
 			ackSocket.setSoTimeout(timeoutLength);
-		//	System.out.println("Trying to receive ack");
-
 			ackSocket.receive(acknowledged);
 			ackSocket.setSoTimeout(0);
 
-
 			byte[] test = acknowledged.getData();
-//			ByteBuffer acknoBuffer = ByteBuffer.wrap(test,4,4);
-//			int tempAckno = acknoBuffer.getInt();
-//			System.out.println("tempAckno: " + tempAckno);
 			ByteBuffer chksumBufferTest = ByteBuffer.wrap(test,0,2);
+			
 			short tempChksum = chksumBufferTest.getShort();
 			
-			
-			
-		//	System.out.println("ack was received");
-		//	System.out.println("ack chksum received: " + tempChksum);
-			
 			if (tempChksum == 1 ) {
+				
 				System.out.println("AckRcvd " + seqno + " ErrAck.");
 				return false;
+				
 			}
 			
 			//Getting bytes to test
@@ -304,48 +256,57 @@ public class UDPClient {
 			
 			ByteBuffer acknoBuffer = ByteBuffer.wrap(test,4,4);
 			int tempAckno = acknoBuffer.getInt();
-		//	System.out.println("tempAckno: " + tempAckno);
-		//	System.out.println("numPackets: " + seqno);
-
-			
 			
 			if(tempAckno == seqno+1){
+				
 				System.out.println("AckRcvd " + seqno + " MoveWnd");
 				return true;			// negates loop's pre-test condition
+				
 			}
 			
 		}catch(IOException e) {
+			
 			if(e instanceof SocketTimeoutException) {
+				
 				System.out.println("TimeOut " + seqno);
 				return false;
+				
 			}
-//			e.printStackTrace();
 		}
 		
 		return false;
+		
 	}
 	
-	
+	/**
+	 * Creates a header to be appended to the beginning of data packet to be
+	 * sent to the receiver.
+	 * 
+	 * @return buf
+	 */
 	public byte[] createHeader() { //partially dont know whats going on
+		
 		ByteBuffer buf = ByteBuffer.allocate(HEADER_SIZE);
 		
 		// create and add chksum header
 		byte[] chksumHeader = new byte[2];
 		ByteBuffer chksumBuf = ByteBuffer.wrap(chksumHeader);
 		chksumBuf.putShort(proxy());
-//		chksumBuf.putShort((short)1);
 
 		chksumBuf.rewind();
+		
 		// create and add chksum header
 		byte[] lenHeader = new byte[2];
 		ByteBuffer lenBuf = ByteBuffer.wrap(lenHeader);
 		lenBuf.putShort(len);
 		lenBuf.rewind();
+		
 		// create and add chksum header
 		byte[] acknoHeader = new byte[4];
 		ByteBuffer acknoBuf = ByteBuffer.wrap(acknoHeader);
 		acknoBuf.putInt(ackno);
 		acknoBuf.rewind();
+		
 		// create and add chksum header
 		byte[] seqnoHeader = new byte[4];
 		ByteBuffer seqnoBuf = ByteBuffer.wrap(seqnoHeader);
@@ -361,18 +322,23 @@ public class UDPClient {
 		// add seqno header to full header
 		buf.put(seqnoHeader);
 		
-		
 		buf.rewind();
 		
 		return buf.array();
 		
 	}
 	
+	/**
+	 * appends the header to the beginning of the data packets array that will
+	 * be sent to the receiver.
+	 */
 	public void addHeader() {//idk whats going on //Just modifying global variables
+		
 		ByteBuffer buf = ByteBuffer.wrap(packetBuffer);
 		
 		buf.put(headerBuffer);
 		buf.put(fileBuffer);
+		
 	}
 	
 }
